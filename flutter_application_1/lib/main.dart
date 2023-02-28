f52f4e5674b95415bea5851d7509e359a2279a2b
Wissaall <lw_messikh@esi.dz>
2023-02-28 01:58:26 +0100

    Positioned Header and text inside the page and the logo icon in the appbar. Added Montserrat font files to the fonts directory.

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,23 +1,61 @@
 import 'package:flutter/material.dart'; //imports dart
 
 //import 'package:flutter_svg/flutter_svg.dart';
-void main() => runApp(MaterialApp(home: Home()));
+void main() => runApp(MaterialApp(
+    title: 'Fluid', theme: ThemeData(fontFamily: 'Montserrat'), home: Home()));
 
 class Home extends StatelessWidget {
   @override //redefinition of the build widget
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Image.asset('assets/logo.png'),
+        flexibleSpace: Container(
+          alignment: Alignment.topLeft,
+          constraints: BoxConstraints.expand(),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("logo.png"), fit: BoxFit.cover),
+          ),
+        ),
+        title: Image.asset(
+          'assets/logo2.png',
+          height: 100,
+          width: 100,
+        ),
         backgroundColor: Color(0xffddecf6),
       ),
       body: Container(
-          decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/hex.jpg"),
-          fit: BoxFit.cover,
+        constraints: BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/hex.jpg"), fit: BoxFit.cover),
         ),
-      )),
+        child: Stack(
+          children: <Widget>[
+            Positioned.fill(
+              top: 100,
+              child: Text(
+                'LOREM IPSUM',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  //  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                ),
+              ),
+            ),
+            Positioned.fill(
+                top: 190,
+                child: Text(
+                  'Lorem ipsum dolor sic mundus creatus est',
+                  softWrap: true,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                )),
+          ],
+        ),
+      ),
     );
   }
 }
