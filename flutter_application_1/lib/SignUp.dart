15444f7d861f0a35ffb5d9974db240164c198f84
minaiscoding <khadir04kh@gmail.com>
2023-03-03 18:49:41 +0100

    added the sign up page

---
+++ flutter_application_1/lib/SignUp.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF4675C0),
+      body: Stack(
+        children: [
+          Positioned(
+            left: 95,
+            top: 72,
+            child: Text(
+              'Sign Up',
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.w700,
+                fontSize: 61,
+                color: Colors.white,
+                height: 1,
+              ),
+            ),
+          ),
+          Positioned(
+            right: 0,
+            bottom: 0,
+            child: Container(
+              width: 392,
+              height: 650,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(70),
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: 60,
+                  left: 30,
+                  right: 30,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'Full name',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                        height: 1.2,
+                        color: Color(0xFF4675C0),
+                      ),
+                    ),
+                    TextField(
+                      controller: _phoneNumberController,
+                      decoration: InputDecoration(
+                        hintText: 'Mohammad Djabed',
+                        hintStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Color(0xFF9FA5C0),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFF4675C0), width: 2),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFF4675C0), width: 2),
+                        ),
+                        prefixIcon: Icon(Icons.face, color: Color(0xFF4675C0)),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Text(
+                      'Phone number',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                        height: 1.2,
+                        color: Color(0xFF4675C0),
+                      ),
+                    ),
+                    TextField(
+                      controller: _phoneNumberController,
+                      decoration: InputDecoration(
+                        hintText: '0000000000',
+                        hintStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Color(0xFF9FA5C0),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFF4675C0), width: 2),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFF4675C0), width: 2),
+                        ),
+                        prefixIcon: Icon(Icons.phone, color: Color(0xFF4675C0)),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Text(
+                      'Password',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                        height: 1.2,
+                        color: Color(0xFF4675C0),
+                      ),
+                    ),
+                    TextField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: '*********',
+                        hintStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Color(0xFF9FA5C0),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFF4675C0), width: 2),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFF4675C0), width: 2),
+                        ),
+                        prefixIcon: Icon(Icons.lock, color: Color(0xFF4675C0)),
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    Text(
+                      'Confirm Password',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                        height: 1.2,
+                        color: Color(0xFF4675C0),
+                      ),
+                    ),
+                    TextField(
+                      controller: _phoneNumberController,
+                      decoration: InputDecoration(
+                        hintText: '*********',
+                        hintStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Color(0xFF9FA5C0),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFF4675C0), width: 2),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFF4675C0), width: 2),
+                        ),
+                        prefixIcon: Icon(Icons.lock, color: Color(0xFF4675C0)),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF4675C0),
+                        shadowColor: Color(0xFF1A2A3A),
+                        elevation: 8,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'You already have an account? sign in',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 15,
+                        height: 1.2,
+                        color: Color.fromARGB(255, 16, 51, 105),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart'; //imports dart
 import 'package:hexcolor/hexcolor.dart';
 import 'Home.dart';
 import 'SignIn.dart';
+import 'SignUp.dart';
 
 void main() => runApp(MaterialApp(
     debugShowCheckedModeBanner: false, //removes debugger banner from App
     title: 'Fluid',
     theme: ThemeData(
         fontFamily: 'Montserrat'), //set the font for the app to Aquire
-    home: LoginPage()));
+    home: SignUpPage()));
