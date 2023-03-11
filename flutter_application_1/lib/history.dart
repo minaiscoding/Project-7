b386c5b7c21d285c809760f26f8842cb4f17fc73
minaiscoding <khadir04kh@gmail.com>
2023-03-11 15:16:29 +0100

    added the menu

--- flutter_application_1/lib/history.dart
+++ flutter_application_1/lib/history.dart
@@ -177,31 +177,197 @@ class _HistoryPageState extends State<HistoryPage> {
               child: GestureDetector(
                 onTapDown: (details) {
                   _openMenu();
-                  showMenu(
-                    context: context,
-                    position: RelativeRect.fromLTRB(309, 46, 0, 0),
-                    items: [
-                      PopupMenuItem(
-                        child: Text('Option 1'),
-                        value: 'Option 1',
-                      ),
-                      PopupMenuItem(
-                        child: Text('Option 2'),
-                        value: 'Option 2',
-                      ),
-                      PopupMenuItem(
-                        child: Text('Option 3'),
-                        value: 'Option 3',
-                      ),
-                    ],
-                  ).then((value) => _closeMenu());
                 },
                 child: BackdropFilter(
+                  child: Icon(Icons.menu, size: 32, color: Color(0xFF1A2A3A)),
                   filter: ImageFilter.blur(
                       sigmaX: _isMenuOpen ? 5.0 : 0,
                       sigmaY: _isMenuOpen ? 5.0 : 0),
-                  child: Icon(Icons.menu, size: 32, color: Color(0xFF1A2A3A)),
                 ),
+              ),
+            ),
+            Visibility(
+              visible:
+                  _isMenuOpen, // Show the stack if showStack is true, hide it if false
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.fromLTRB(59, 142, 0, 0),
+                    width: 273,
+                    height: 490,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Color.fromRGBO(26, 42, 58, 0.25),
+                        width: 1,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(101, 160, 255, 0.5),
+                          offset: Offset(0, 8),
+                          blurRadius: 30,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(33),
+                    ),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 40),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Container(
+                            width: 121,
+                            height: 56,
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Home',
+                              style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 24,
+                                height: 1,
+                                color: Color(0XFF21457D),
+                              ),
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 28),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Container(
+                            width: 121,
+                            height: 56,
+                            alignment: Alignment.center,
+                            child: Text(
+                              'History',
+                              style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 24,
+                                height: 1,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0XFF21457D),
+                            elevation: 8,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            shadowColor: Color(0XFF21457D),
+                          ),
+                        ),
+                        SizedBox(height: 28),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Container(
+                            width: 121,
+                            height: 56,
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Settings',
+                              style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 24,
+                                height: 1,
+                                color: Color(0XFF21457D),
+                              ),
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 28),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Container(
+                            width: 121,
+                            height: 56,
+                            alignment: Alignment.center,
+                            child: Text(
+                              'FAQ',
+                              style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 24,
+                                height: 1,
+                                color: Color(0XFF21457D),
+                              ),
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 28),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Container(
+                            width: 70,
+                            height: 33,
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Log out',
+                              style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 17.33333,
+                                height: 1,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0XFF21457D),
+                            elevation: 8,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            shadowColor: Color(0XFF21457D),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 150,
+                    right: 20,
+                    child: IconButton(
+                      icon: Icon(Icons.close),
+                      iconSize: 40,
+                      color: Color(0XFF21457D),
+                      onPressed: () {
+                        setState(() {
+                          _isMenuOpen =
+                              false; // Set showStack to false to hide the stack
+                        });
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
